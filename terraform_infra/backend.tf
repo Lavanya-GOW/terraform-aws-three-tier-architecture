321c820e11ed31f3438355b782b13476555afe97
LavanyaGOW <sharmalavanya1710@gmail.com>
2026-06-30 01:29:24 +0000

    Configured backend and fixed some plan and apply errors

--- terraform_infra/backend.tf
+++ terraform_infra/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "my-first-3-tier-architecture"
+    bucket         = "my-first-3-tier-architecture-s3-backend-bucket"
     key            = "production/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "3-tier-architecture"
+    dynamodb_table = "3-tier-architecture-table"
+    
   }
 }
